b7129c489ce3bf0ca1639105cab843041ab83eae
Halim Jabbes <96540322+hxlxmjxbbxs@users.noreply.github.com>
2024-06-26 21:03:28 -0400

    Update gcid-extractor.py V2

--- gcid-extractor.py
+++ gcid-extractor.py
@@ -6,6 +6,7 @@ import os
 import argparse
 from tqdm import tqdm
 from colorama import init, Fore, Style
+import jsbeautifier
 
 init(autoreset=True)
 
@@ -19,7 +20,7 @@ def extract_origin(url):
     parsed_url = urllib.parse.urlparse(url)
     query_params = urllib.parse.parse_qs(parsed_url.query)
     redirect_uri = query_params.get('redirect_uri', [None])[0]
-    if redirect_uri:
+    if (redirect_uri):
         redirect_uri_parsed = urllib.parse.urlparse(urllib.parse.unquote(redirect_uri))
         return redirect_uri_parsed.netloc
     return "Unknown"
@@ -33,75 +34,137 @@ def extract_email(response_text):
     match = email_pattern.search(response_text)
     return match.group(0) if match else None
 
+def extract_all_domains(url, response_text):
+    domains = set()
+    url_pattern = re.compile(r'https://[a-zA-Z0-9.-]+')
+    domain_pattern = re.compile(r'@([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})')
+
+    url_matches = url_pattern.findall(url)
+    for match in url_matches:
+        domain = match.split("//")[-1]
+        domains.add(domain)
+
+    parsed_url = urllib.parse.urlparse(url)
+    query_params = urllib.parse.parse_qs(parsed_url.query)
+    for key, values in query_params.items():
+        for value in values:
+            if '://' in value:
+                sub_parsed_url = urllib.parse.urlparse(value)
+                domains.add(sub_parsed_url.netloc)
+
+    email = extract_email(response_text)
+    if email:
+        domain_match = domain_pattern.search(email)
+        if domain_match:
+            domains.add(domain_match.group(1))
+
+    domains.discard("accounts.google.com")
+    domains.discard("www.googleapis.com")
+    domains.discard("valid.url")
+    domains.discard("")
+    
+    return domains
+
+def beautify_js(js_code):
+    opts = jsbeautifier.default_options()
+    opts.indent_size = 2
+    return jsbeautifier.beautify(js_code, opts)
+
+def extract_js_domains(response_text):
+    lines = response_text.split('\n')
+    last_10_lines = '\n'.join(lines[-10:])
+    beautified_js = beautify_js(last_10_lines)
+
+    domain_pattern = re.compile(r'https?://([a-zA-Z0-9.-]+)')
+    domains = set(domain_pattern.findall(beautified_js))
+
+    return domains
+
 def process_url(url):
     client_id = extract_client_id(url)
-    origin = extract_origin(url)
+    success = True
 
     if not client_id:
         print(f"{Fore.RED}No client_id found in the URL.")
-        return
+        success = False
+    elif not is_valid_client_id(client_id):
+        print(f"{Fore.RED}Invalid client_id: {client_id}")
+        success = False
 
-    if not is_valid_client_id(client_id):
-        print(f"{Fore.RED}Invalid client_id format.")
-        return
+    if success:
+        headers = {
+            "Host": "accounts.google.com",
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0",
+            "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+            "Cookie": "__Host-GAPS=1:qo5tjYVkpfzrF3EH0jRHUndSlYgjVw:XEtC-4BjS1v1g_5D;",
+        }
 
-    headers = {
-        "Host": "accounts.google.com",
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0",
-        "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
-        "Cookie": "__Host-GAPS=1:qo5tjYVkpfzrF3EH0jRHUndSlYgjVw:XEtC-4BjS1v1g_5D;",
-    }
+        data = f'f.req=%5B%5B%5B%22WZfWSd%22%2C%22%5B2%2C1%5D%22%2Cnull%2C%221%22%5D%2C%5B%22etGTrd%22%2C%22%5B%5C%22{client_id}%5C%22%2C%5C%22https%3A%2F%2Fvpn1.volans.tech%5C%22%5D%22%2Cnull%2C%222%22%5D%2C%5B%22Aho3hb%22%2C%22%5B%5D%22%2Cnull%2C%223%22%5D%2C%5B%22i3kFoc%22%2C%22%5B%5D%22%2Cnull%2C%224%22%5D%2C%5B%22zKAP2e%22%2C%22%5B%5C%22identity-signin-password%5C%22%5D%22%2Cnull%2C%226%22%5D%2C%5B%22RzSO2e%22%2C%22%5B%5C%22{client_id}%5C%22%5D%22%2Cnull%2C%227%22%5D%5D%5D&at=ALt4Ve29PZzUxNk6P93qVlYfDpdE%3A1719387079176&'
 
-    data = f'f.req=%5B%5B%5B%22WZfWSd%22%2C%22%5B2%2C1%5D%22%2Cnull%2C%221%22%5D%2C%5B%22etGTrd%22%2C%22%5B%5C%22{client_id}%5C%22%2C%5C%22https%3A%2F%2Fvpn1.volans.tech%5C%22%5D%22%2Cnull%2C%222%22%5D%2C%5B%22Aho3hb%22%2C%22%5B%5D%22%2Cnull%2C%223%22%5D%2C%5B%22i3kFoc%22%2C%22%5B%5D%22%2Cnull%2C%224%22%5D%2C%5B%22zKAP2e%22%2C%22%5B%5C%22identity-signin-password%5C%22%5D%22%2Cnull%2C%226%22%5D%2C%5B%22RzSO2e%22%2C%22%5B%5C%22{client_id}%5C%22%5D%22%2Cnull%2C%227%22%5D%5D%5D&at=ALt4Ve29PZzUxNk6P93qVlYfDpdE%3A1719387079176&'
+        try:
+            response = requests.post(
+                "https://accounts.google.com/v3/signin/_/AccountsSignInUi/data/batchexecute",
+                headers=headers,
+                data=data
+            )
 
-    response = requests.post(
-        "https://accounts.google.com/v3/signin/_/AccountsSignInUi/data/batchexecute?rpcids=WZfWSd%2CetGTrd%2CAho3hb%2Ci3kFoc%2CzKAP2e%2CRzSO2e&source-path=%2Fv3%2Fsignin%2Fchallenge%2Fpwd&f.sid=-4453641016717953570&bl=boq_identityfrontendauthuiserver_20240609.08_p0&hl=en-US&TL=AC3PFD5op1jFLF1g6M_Z1HG3lRBSFNhzNp-HToRplZO5KeTgWLcfi8zD_d-uvsTd&_reqid=212684&rt=c",
-        headers=headers,
-        data=data
-    )
+            if response.status_code == 200:
+                response_text = response.text
+                email = extract_email(response_text)
+                domains = extract_all_domains(url, response_text)
+                js_domains = extract_js_domains(response_text)
+                domains.update(js_domains)
 
-    if response.status_code == 200:
-        email = extract_email(response.text)
-        if email:
-            print(f"{Fore.GREEN}{Style.BRIGHT}Client ID: {Style.RESET_ALL}{client_id}")
-            print(f"{Fore.GREEN}{Style.BRIGHT}Domain: {Style.RESET_ALL}{origin}")
-            print(f"{Fore.GREEN}{Style.BRIGHT}Admin Email: {Style.RESET_ALL}{email}")
-        else:
+                if email:
+                    print(f"{Fore.GREEN}{Style.BRIGHT}Client ID: {Style.RESET_ALL}{client_id}")
+                    print(f"{Fore.GREEN}{Style.BRIGHT}Admin Email: {Style.RESET_ALL}{email}")
+                else:
+                    print(f"{Fore.RED}{Style.BRIGHT}Client ID: {Style.RESET_ALL}{client_id}")
+                    print(f"{Fore.RED}No email address found in the response.")
+
+                if domains:
+                    print(f"{Fore.GREEN}{Style.BRIGHT}Domains: {Style.RESET_ALL}{', '.join(domains)}")
+                else:
+                    print(f"{Fore.RED}No domains found in the response.")
+
+                print()
+
+            else:
+                print(f"{Fore.RED}{Style.BRIGHT}Client ID: {Style.RESET_ALL}{client_id}")
+                print(f"{Fore.RED}Failed to fetch the email. Status Code: {response.status_code}")
+
+        except requests.exceptions.RequestException as e:
             print(f"{Fore.RED}{Style.BRIGHT}Client ID: {Style.RESET_ALL}{client_id}")
-            print(f"{Fore.RED}{Style.BRIGHT}Domain: {Style.RESET_ALL}{origin}")
-            print(f"{Fore.RED}No email address found in the response.")
+            print(f"{Fore.RED}Error fetching the email: {e}")
+
+def process_client_id(client_id):
+    if client_id and is_valid_client_id(client_id):
+        url = f"https://accounts.google.com/gsi/select?client_id={client_id}&redirect_uri=https://valid.url"
+        process_url(url)
+    elif client_id:
+        print(f"{Fore.RED}Invalid client_id: {client_id}")
+
+def process_input(input_value):
+    if os.path.isfile(input_value):
+        with open(input_value, 'r') as file:
+            lines = file.readlines()
+        for line in tqdm(lines, desc="Validating client IDs and URLs", colour="yellow"):
+            line = line.strip()
+            if "http" in line:
+                process_url(line)
+            else:
+                process_client_id(line)
+    elif "http" in input_value:
+        process_url(input_value)
     else:
-        print(f"{Fore.RED}{Style.BRIGHT}Client ID: {Style.RESET_ALL}{client_id}")
-        print(f"{Fore.RED}{Style.BRIGHT}Domain: {Style.RESET_ALL}{origin}")
-        print(f"{Fore.RED}Failed to fetch the email. Status Code: {response.status_code}")
+        process_client_id(input_value)
 
 def main():
-    parser = argparse.ArgumentParser(description="Extract and validate Google Client IDs from URLs.")
-    parser.add_argument("url", nargs="?", help="URL to extract client_id from")
-    parser.add_argument("--input-file", help="File containing client_id to validate")
+    parser = argparse.ArgumentParser(description="Extract and validate Google Client IDs from URLs or files.")
+    parser.add_argument("input", help="URL, client_id or file containing URLs/client_ids to validate")
     args = parser.parse_args()
 
-    if args.input_file:
-        if not os.path.isfile(args.input_file):
-            print(f"{Fore.RED}The file {args.input_file} does not exist.")
-            sys.exit(1)
-
-        with open(args.input_file, 'r') as file:
-            lines = file.readlines()
-
-        for line in tqdm(lines, desc="Validating client IDs"):
-            client_id = line.strip()
-            if is_valid_client_id(client_id):
-                url = f"https://accounts.google.com/gsi/select?client_id={client_id}&redirect_uri=https%3A%2F%2Fexample.com"
-                process_url(url)
-            else:
-                print(f"{Fore.RED}Invalid client_id: {client_id}")
-
-    elif args.url:
-        process_url(args.url)
-    else:
-        print(f"{Fore.RED}Usage: gcid-extractor.py <URL> or gcid-extractor.py --input-file <file>")
-        sys.exit(1)
+    process_input(args.input)
 
 if __name__ == "__main__":
     main()
